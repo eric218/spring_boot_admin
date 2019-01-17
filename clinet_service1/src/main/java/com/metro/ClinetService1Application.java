fbb960baa537f0e2921472058ba0a88ebe93a500
wangwei <eric218@163.com>
2019-01-17 17:03:14 +0800

    add

--- clinet_service1/src/main/java/com/metro/ClinetService1Application.java
+++ clinet_service1/src/main/java/com/metro/ClinetService1Application.java
@@ -1,9 +1,10 @@
 package com.metro;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+@EnableAdminServer
 @EnableEurekaClient
 @SpringBootApplication
 public class ClinetService1Application {
