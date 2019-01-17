a9f8588532778ec9b6a69d4a2f69dd2c121fc271
wangwei <eric218@163.com>
2019-01-17 15:30:25 +0800

    add

---
+++ client_service3/src/main/java/com/metro/client_service3/ClientService3Application.java
@@ -0,0 +1,13 @@
+package com.metro.client_service3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ClientService3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientService3Application.class, args);
+    }
+
+}
+
